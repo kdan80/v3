a06675b459c767c0db9cc5525278b4d9bb81193d
kdan80 <kierandansey80@gmail.com>
2022-06-21 20:09:13 +0100

    Tidied up tsconfig path mapping

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import type { NextPage } from 'next'
-import Layout from '@components/Layout'
+import { Layout } from '@components'
+import  { About, Landing } from '@sections'
 import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
     <Layout>
       Hello world
+      <About />
+      <Landing />
     </Layout>
   )
 }
