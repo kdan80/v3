5bfe96d43993980f3789f5fa5957648f367d742e
kdan80 <kierandansey80@gmail.com>
2022-06-25 19:36:50 +0100

    Added gray-matter and used getStaticProps to pull in markdown

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,22 +2,48 @@ import type { NextPage } from 'next'
 import { Layout } from '@components'
 import  { About, Contact, Landing } from '@sections'
 import { useRouter } from 'next/router'
+import fs from 'fs'
+import matter from 'gray-matter'
+import Image from 'next/image'
+import Link from 'next/link'
 
 interface Props {
   viewportHeight: number
+  projects: any
 }
 
-const Home: NextPage<Props> = ({viewportHeight}) => {
+const Home: NextPage<Props> = ({viewportHeight, projects}) => {
 
-  const { pathname } = useRouter();
+    const { pathname } = useRouter();
 
-  return (
-    <Layout location={pathname} viewportHeight={viewportHeight} >
-      <Landing viewportHeight={viewportHeight} />
-      <About viewportHeight={viewportHeight} />
-      <Contact viewportHeight={viewportHeight} />
-    </Layout>
-  )
+    return (
+        <Layout location={pathname} viewportHeight={viewportHeight} >
+            <Landing viewportHeight={viewportHeight} />
+            <About viewportHeight={viewportHeight} />
+            <Contact viewportHeight={viewportHeight} />
+        </Layout>
+    )
 }
 
 export default Home
+
+export async function getStaticProps(){
+
+    const files = fs.readdirSync('content/featured');
+
+    const projects = files.map((file) => {
+        const readFile = fs.readFileSync(`content/featured/${file}/index.md`, 'utf-8');
+        const { data: frontmatter } = matter(readFile);
+        return {
+            frontmatter,
+        };
+    });
+
+    console.log("projects: ", projects);
+    
+    return{
+        props: {
+          files,
+        }
+    }
+}
