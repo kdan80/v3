ed850216d45136541bb756986d6e0d24a83d7902
kdan80 <kierandansey80@gmail.com>
2022-06-28 18:41:40 +0100

    Change Logo.tsx -> Logo.jsx due to typescript issue

--- components/icons/Logo.jsx
+++ components/icons/Logo.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import anime from 'animejs';
 
+// TODO: refactor in orded to work with typescript
+
 const StyledSvg = styled.svg`
     height: 5rem;
 
