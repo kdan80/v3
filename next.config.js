87f0f9d42e422b64f3fc64d6428c5db594b8795d
kdan80 <kierandansey80@gmail.com>
2022-06-29 15:34:21 +0100

    Added custom _document.tsx required to achieve ssr with styled comps

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
   compiler: {
     styledComponents: true,
+    displayName: true,
     ssr: true
   }
 }
