0074023ae07b879a30e50f6dec66e239d4ff8e4c
kdan80 <kierandansey80@gmail.com>
2022-07-03 19:19:47 +0100

    Removed html tag from head

--- components/DocumentHead.tsx
+++ components/DocumentHead.tsx
@@ -31,7 +31,6 @@ const DocumentHead: React.FC<Props> = ({ title, description, image }) => {
 
     return (
         <Head>
-            <html lang='en' />
             <meta charSet='utf-8' />
             <title>{siteMetadata.title}</title>
             
@@ -39,7 +38,7 @@ const DocumentHead: React.FC<Props> = ({ title, description, image }) => {
             <meta name='image' content={siteMetadata.image} />
             <meta name='theme-color' content='#151618'/>
 
-            <link rel='icon' type='image/x-icon' href='/images/favicons/favicon.ico' />
+            <link rel='icon' href='/images/favicons/favicon.ico?v=1.0' />
             <link rel='apple-touch-icon' sizes='180x180' href='/images/favicons/apple-touch-icon.png' />
             <link rel='icon' type='image/png' sizes='16x16' href='/images/favicons/favicon-16x16.png' />
             <link rel='icon' type='image/png' sizes='32x32' href='/images/favicons/favicon-32x32.png' />
